fda53c2f5d45af2b5b678343dcd961e3a32ae077
omrakibe <rakibeom30@gmail.com>
2025-01-31 15:36:56 +0530

    Hello

--- BackEnd/Contoller/Analytics.js
+++ BackEnd/Contoller/Analytics.js
@@ -1,7 +1,9 @@
 const lineData = require("../models/analyticsModel");
 
-const getAnalyticsData = (req, res) => {
+const getAnalyticsData = (req, res) => { //get api
   res.json(lineData);
 };
 
 module.exports = { getAnalyticsData };
+
+

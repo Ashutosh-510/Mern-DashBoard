74df7db364769b48b36e64cd7e986a5d89bf30ca
Ashutosh Singh <ashutoshsinghu2002@gmail.com>
2025-01-31 15:22:29 +0530

    Mern

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 const analyticsRoutes = require("./routes/analyticsRoutes");
 
+//
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);

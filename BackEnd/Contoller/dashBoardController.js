8c5216aa194a8040d122808a2e5128d033c10da6
Ashutosh Singh <ashutoshsinghu2002@gmail.com>
2025-02-01 11:31:58 +0530

    New ROUTES

--- BackEnd/Contoller/dashBoardController.js
+++ BackEnd/Contoller/dashBoardController.js
@@ -32,4 +32,38 @@ const getEmployeeData = async (req, res) => {
   }
 };
 
-module.exports = { getData, getEmployeeData };
+const getCandidateData = async (req, res) => {
+  try {
+    const CandidateData = {
+      NewCandidate: 12,
+      TotalCandidate: 2000,
+      ActiveCandidate: 400,
+      CandidateHired: 1000,
+    };
+
+    res.status(200).json({ message: CandidateData });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+const getJobPostedData = async (req, res) => {
+  try {
+    const JobpostedData = {
+      NewJobPosted: 3000,
+      TotalJobPosted: 3000,
+      TotalActiveJobs: 500,
+      ExpiredJobs: 2500,
+    };
+    res.status(200).json({ message: JobpostedData });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getData,
+  getEmployeeData,
+  getCandidateData,
+  getJobPostedData,
+};
--- BackEnd/Routes/dashboardRoutes.js
+++ BackEnd/Routes/dashboardRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const dashBoardController = require("../Contoller/dashBoardController.js");
-const EmployeeDashBoard = require("../Contoller/EmployeeDashBoard.js");
+//const EmployeeDashBoard = require("../Contoller/EmployeeDashBoard.js");
 
 router.get("/", dashBoardController.getData);
 router.route("/Employer").get(dashBoardController.getEmployeeData);
+router.route("/Candidate").get(dashBoardController.getCandidateData);
+router.route("/JobPosted").get(dashBoardController.getJobPostedData);
 module.exports = router;
